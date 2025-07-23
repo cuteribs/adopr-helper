17aca6264bf69280e9f8aaf65796497b79c7ee20
cuteribs <85863433+cuteribs@users.noreply.github.com>
2025-07-23 17:46:17 +0800

    go

--- src/test/extension.test.js
+++ src/test/extension.test.js
@@ -1,14 +1,15 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var assert = require("assert");
+import * as assert from 'assert';
+
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-var vscode = require("vscode");
+import * as vscode from 'vscode';
 // import * as myExtension from '../../extension';
-suite('Extension Test Suite', function () {
-    vscode.window.showInformationMessage('Start all tests.');
-    test('Sample test', function () {
-        assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-        assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-    });
+
+suite('Extension Test Suite', () => {
+	vscode.window.showInformationMessage('Start all tests.');
+
+	test('Sample test', () => {
+		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
+		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+	});
 });
